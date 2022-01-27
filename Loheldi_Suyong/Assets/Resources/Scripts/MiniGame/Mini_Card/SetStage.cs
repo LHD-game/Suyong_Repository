b0bb5c5f9707d7339cd4cdd8069ec6bc0b424fc5
BWKAI <94029466+BWKAI@users.noreply.github.com>
2022-01-27 11:33:43 +0900

    6

---
+++ Loheldi_Suyong/Assets/Resources/Scripts/MiniGame/Mini_Card/SetStage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStage : MonoBehaviour
+{
+
+    static public string[][] stage = new string[][]
+    {
+            new string[]
+            {
+                " ***. ",
+                " *.*. ",
+                " ***. ",
+                " .... "
+            },
+            new string[]
+            {
+                " ****. ",
+                " ****. ",
+                " ****. ",
+                " ****. ",
+                " ..... "
+            },
+            new string[]
+            {
+                " ****. ",
+                " ****. ",
+                " ****. ",
+                " ****. ",
+                " ..... "
+            },
+    };
+}
