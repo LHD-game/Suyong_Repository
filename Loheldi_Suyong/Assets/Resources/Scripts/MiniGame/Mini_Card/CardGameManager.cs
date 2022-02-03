f376735d1d28c11f9c3438f737c9a86fd2aa42ed
BWKAI <94029466+BWKAI@users.noreply.github.com>
2022-02-03 19:40:29 +0900

    3

--- Loheldi_Suyong/Assets/Resources/Scripts/MiniGame/Mini_Card/CardGameManager.cs
+++ Loheldi_Suyong/Assets/Resources/Scripts/MiniGame/Mini_Card/CardGameManager.cs
@@ -8,33 +8,35 @@ using UnityEngine.UI;
 
 public class CardGameManager : MonoBehaviour
 {
-    public List<GameObject> CardList = new List<GameObject>();
-    public List<GameObject> TempCardList = new List<GameObject>();
-    private List<GameObject> MixCardList = new List<GameObject>();
-    private List<GameObject> AllCard = new List<GameObject>();
+    GameObject[] cards;     //카드를 프리펩에서 저장
+    GameObject[] cardsMix;  //cards 배열에서 섞은 것을 저장
+    int[] level = { 0, 4, 6, 8 };      //레벨 별 카드 종류 개수
+    float[] timerLen = { 0f, 64f, 64f, 54f };
+    public static float timer;
+    private List<GameObject> AllCard = new List<GameObject>();    //생성된 카드 오브젝트
 
     public static GameObject OpenCard;
     public static GameObject LastCard;
     public static bool GameStart;
-    public GameObject FndText;
-    public GameObject EndText;
-    public GameObject EndButton;
 
     public Slider TimeSlider;
+    private bool isPause = false; //true일때 pause 상태
 
     public GameObject WelcomePanel;
+    public GameObject GameOverPanel;
+    public GameObject PausePanel;
 
     int cardCnt;
     int flipCnt = 0;
 
     static public int stageNum = 1;
-    int stageCount = 3;
-    public static float Timer = 64f;
+    [SerializeField]
+    private Text stageTxt;
     public Text timeText;
 
     int a;
 
-    public enum STATE
+    public enum STATE   //현재 게임 상태 저장
     {
         START, HIT, WAIT, IDLE, CLEAR, FAIL
     };
@@ -42,71 +44,108 @@ public class CardGameManager : MonoBehaviour
 
     void Start()
     {
-        state = STATE.START;
-        GameStart = false;
-        a = 0;
+        Welcome();
     }
 
     void Update()
     {
-        if (GameStart)
+        if (GameStart && !isPause)
         {
             switch (state)
             {
-                case STATE.START:
-                    SetUp();
+                case STATE.START:   //게임 시작
+                    CardSet();
                     break;
-                case STATE.HIT:
+                case STATE.HIT:     //카드 눌렀을 때
                     CheckCard();
                     break;
-                case STATE.CLEAR:
+                case STATE.CLEAR:   //한 스테이지 클리어
                     StartCoroutine(StageClear());
                     break;
-                case STATE.WAIT:
-                    Timer -= Time.deltaTime;
+                case STATE.WAIT:    //카드를 열고 기다리는 상태
+                    timer -= Time.deltaTime;
                     break;
-                case STATE.FAIL:
+                case STATE.FAIL:    //시간이 다 되어 게임오버
                     StageFail();
                     break;
-                case STATE.IDLE:
-                    Timer -= Time.deltaTime;
+                case STATE.IDLE:    //기본 상태
+                    timer -= Time.deltaTime;
                     break;
             }
-            if (stageNum == 3)
+
+            if (stageNum < 4)
             {
-                if (Timer <= 50f)
+                if (timer <= timerLen[stageNum])
                 {
-                    timeText.text = string.Format("시간: {0:N0}", Timer);
+                    timeText.text = string.Format("시간: {0:N0}", timer);
                 }
                 else
                 {
-                    a = 50;
+                    a = (int)timerLen[stageNum] - 4;
                     timeText.text = "시간: " + a;
                 }
-            }
-            else
-            {
-                if (Timer <= 60f)
+
+                if (timer <= 0)
                 {
-                    timeText.text = string.Format("시간: {0:N0}", Timer);
+                    state = STATE.FAIL;
                 }
-                else
-                {
-                    a = 60;
-                    timeText.text = "시간: " + a;
-                }
+                TimeSlider.maxValue = a;
+                TimeSlider.value = timer;
             }
-            if (Timer <= 0)
-            {
-                state = STATE.FAIL;
-            }
-            TimeSlider.maxValue = a;
-            TimeSlider.value = Timer;
+            
         }
-        else
+    }
+
+    public void Welcome()   //게임 초기화 함수
+    {
+        state = STATE.START;
+        GameStart = false;
+        isPause = false;
+        a = 0;
+        cards = Resources.LoadAll<GameObject>("Prefebs/Cards/");    //카드들을 전부 저장한다.
+        stageNum = 1;   //스테이지 1로 초기화
+        timer = timerLen[stageNum];
+
+        WelcomePanel.SetActive(true);
+        GameOverPanel.SetActive(false);
+        PausePanel.SetActive(false);
+        
+
+    }
+
+    void CardSet()  // 스테이지 카드 섞어서 배열에 저장
+    {
+        AllCard.Clear();
+
+        StageText();
+
+        int arrLen = level[stageNum]; //스테이지에 배치될 카드 종류 개수
+        cardCnt = arrLen * 2;   //스테이지에 배치될 카드의 총 개수
+        timer = timerLen[stageNum];
+        GameObject[] arrTmp = new GameObject[cardCnt]; //한 카드 당 두 장씩 저장하게 될 것이므로
+        Debug.Log("arrTmp: " + arrTmp.Length);
+        for(int i = 0; i < arrLen; i++)
         {
-            WelcomePanel.SetActive(true);
+            for (int j = 0; j<2; j++)
+            {
+                while (true)
+                {
+                    int rand = Random.Range(0, arrTmp.Length);
+                    if(arrTmp[rand] == null)    //arrTmp[rand]에 값이 없을 경우, card[i]를 넣는다. 이를 두 번 반복하여 같은 카드가 두 개 저장되도록.
+                    {
+                        arrTmp[rand] = cards[i];
+                        Debug.Log("arrTmp: " + rand + ", card[i]: " + cards[i]);
+                        break;
+                    }
+                }
+
+            }
         }
+
+        cardsMix = new GameObject[arrTmp.Length];   //배열 길이 초기화
+        cardsMix = arrTmp; //섞인 카드를 cardsMix 배열에 넣는다.
+
+        StartCoroutine(MakeStage());
     }
 
     void CheckCard()
@@ -142,82 +181,8 @@ public class CardGameManager : MonoBehaviour
         LastCard = null;
         state = STATE.IDLE;
     }
-    void SetUp()
-    {
-        TempCardList.Clear();
-        AllCard.Clear();
+    
 
-        if (stageNum == 1)
-        {
-            TempCardList.Add(CardList[0]);
-            TempCardList.Add(CardList[0]);
-            TempCardList.Add(CardList[1]);
-            TempCardList.Add(CardList[1]);
-            TempCardList.Add(CardList[2]);
-            TempCardList.Add(CardList[2]);
-            TempCardList.Add(CardList[3]);
-            TempCardList.Add(CardList[3]);
-            cardCnt = 8;
-            Timer = 64f;
-        }
-        else if (stageNum == 2)
-        {
-            TempCardList.Add(CardList[0]);
-            TempCardList.Add(CardList[0]);
-            TempCardList.Add(CardList[1]);
-            TempCardList.Add(CardList[1]);
-            TempCardList.Add(CardList[2]);
-            TempCardList.Add(CardList[2]);
-            TempCardList.Add(CardList[3]);
-            TempCardList.Add(CardList[3]);
-            TempCardList.Add(CardList[4]);
-            TempCardList.Add(CardList[4]);
-            TempCardList.Add(CardList[5]);
-            TempCardList.Add(CardList[5]);
-            TempCardList.Add(CardList[6]);
-            TempCardList.Add(CardList[6]);
-            TempCardList.Add(CardList[7]);
-            TempCardList.Add(CardList[7]);
-            cardCnt = 16;
-            Timer = 64f;
-        }
-        else if (stageNum == 3)
-        {
-            TempCardList.Add(CardList[0]);
-            TempCardList.Add(CardList[0]);
-            TempCardList.Add(CardList[1]);
-            TempCardList.Add(CardList[1]);
-            TempCardList.Add(CardList[2]);
-            TempCardList.Add(CardList[2]);
-            TempCardList.Add(CardList[3]);
-            TempCardList.Add(CardList[3]);
-            TempCardList.Add(CardList[4]);
-            TempCardList.Add(CardList[4]);
-            TempCardList.Add(CardList[5]);
-            TempCardList.Add(CardList[5]);
-            TempCardList.Add(CardList[6]);
-            TempCardList.Add(CardList[6]);
-            TempCardList.Add(CardList[7]);
-            TempCardList.Add(CardList[7]);
-            //TempCardList.Add(CardList[8]);
-            //TempCardList.Add(CardList[8]);
-            //TempCardList.Add(CardList[9]);
-            //TempCardList.Add(CardList[9]);
-            //TempCardList.Add(CardList[10]);
-            //TempCardList.Add(CardList[10]);
-            //TempCardList.Add(CardList[11]);
-            //TempCardList.Add(CardList[11]);
-            cardCnt = 16;//24;
-            Timer = 54f;
-        }
-        StartCoroutine(MakeStage());
-    }
-    void Clear()
-    {
-        EndText.SetActive(true);
-        EndButton.SetActive(true);
-        Time.timeScale = 0;
-    }
 
     IEnumerator CloseTwoCards()
     {
@@ -233,7 +198,7 @@ public class CardGameManager : MonoBehaviour
     {
         state = STATE.WAIT;
 
-        ++stageNum;
+        stageNum++;
         flipCnt = 0;
 
         yield return new WaitForSeconds(1);
@@ -244,12 +209,17 @@ public class CardGameManager : MonoBehaviour
             state = STATE.WAIT;
         }
     }
-    void StageFail()
-    {
 
-        FndText.SetActive(true);
-        EndButton.SetActive(true);
-        Time.timeScale = 0;
+    void Clear()
+    {
+        GameOverPanel.SetActive(true);
+        GameStart = false;
+    }
+    public void StageFail()
+    {
+        GameStart = false;
+        GameOverPanel.SetActive(true);
+        DestroyAll();
     }
     IEnumerator MakeStage()
     {
@@ -257,8 +227,8 @@ public class CardGameManager : MonoBehaviour
 
         float sx = -2.05f;
         float sz = 3.32f;
-        ShuffleCard();
-        SetCardPos(out sx, out sz);
+
+        SetCardPos(out sx, out sz); //카드 위치 설정 함수
 
         int n = 1;
 
@@ -275,9 +245,9 @@ public class CardGameManager : MonoBehaviour
                 {
                     case '*':
 
-                        GameObject Tempcard = Instantiate(TempCardList[n - 1]);
-                        Tempcard.transform.position = new Vector3(x, 4.84f, sz);
-                        AllCard.Add(Tempcard);
+                        GameObject Tempcard = Instantiate(cardsMix[n-1]);
+                        Tempcard.transform.position = new Vector3(x, 1f, sz);
+                        AllCard.Add(Tempcard);  //세팅된 카드 오브젝트를 AllCard에 저장
 
                         x = x + 1.35f;
                         n++;
@@ -310,7 +280,7 @@ public class CardGameManager : MonoBehaviour
     {
         float x = -2.05f;
         float z = 3.32f;
-        string[] str = SetStage.stage[stageNum - 1];
+        string[] str = SetStage.stage[stageNum-1];
 
         for (int i = 0; i < str.Length; i++)
         {
@@ -340,16 +310,31 @@ public class CardGameManager : MonoBehaviour
             sz = 3.32f;
         }
     }
-    
-    void ShuffleCard()
+
+    private void StageText()
     {
-        int count = TempCardList.Count;
-        for (int i = 0; i < count; i++)
+        stageTxt.text = stageNum + " 단계";
+    }
+
+    public void GamePause() //일시 정지 누르면 isPause = true, 재개 누르면 isPause = false
+    {
+        isPause = !isPause;
+        if (isPause)
         {
-            int rand = Random.Range(0, TempCardList.Count);
-            MixCardList.Add(TempCardList[rand]);
-            TempCardList.RemoveAt(rand);
+            PausePanel.SetActive(true);
         }
-        TempCardList = MixCardList;
+        else
+        {
+            PausePanel.SetActive(false);
+        }
+    }
+
+    void DestroyAll()
+    {
+        for(int i=0; i<AllCard.Count; i++)
+        {
+            Destroy(AllCard[i]);
+        }
+        AllCard.Clear();
     }
 }
--- Loheldi_Suyong/Assets/Resources/Scripts/MiniGame/Mini_Run/RunGameManager.cs
+++ Loheldi_Suyong/Assets/Resources/Scripts/MiniGame/Mini_Run/RunGameManager.cs
@@ -6,22 +6,27 @@ public class RunGameManager : MonoBehaviour
 {
     public static int difficulty = 0;
 
-    public Transform NPC1;
-    public Transform NPC2;
-    public Transform NPC3;
+    public Transform[] NPC = new Transform[3];  //hami, nari, himchan 순서대로
     public Transform Player;
+    private float NPCz;
+    private float Playerz;
+
+    public Rigidbody player;
 
     public Transform NMarker;
     public Transform PMarker;
 
     public GameObject WelcomePanel;
     public GameObject GameOverPanel;
+    public GameObject PausePanel;
     public GameObject RunBtnPanel;
 
     public GameObject Win_txt;
     public GameObject Lose_txt;
 
     private Vector3 markerPos;
+
+    public static bool isPause = false;
     
     void Start()
     {
@@ -29,62 +34,33 @@ public class RunGameManager : MonoBehaviour
         Reset();
 
     }
+
+    private Transform nowNPC;
     void Update()
     {
-        if (difficulty == 1)
+        nowNPC = NPC[difficulty];
+        nowNPC.gameObject.SetActive(true);
+        if(nowNPC.position.z >= 3000)
         {
-            NPC1.gameObject.SetActive(true);
-            if (NPC1.position.z >= 857)
-            {
-                NPC1.gameObject.GetComponent<RunHamiRun>().enabled = false;
-                RunBtnPanel.SetActive(false);
-                Lose_txt.gameObject.SetActive(true);
-                GameOverPanel.SetActive(true);
-            }
-            if (Player.position.z >= 857)
-            {
-                NPC1.gameObject.GetComponent<RunHamiRun>().enabled = false;
-                RunBtnPanel.SetActive(false);
-                Win_txt.gameObject.SetActive(true);
-                GameOverPanel.SetActive(true);
-            }
+            nowNPC.gameObject.GetComponent<RunNPC>().enabled = false;
+            RunBtnPanel.SetActive(false);
+            Lose_txt.gameObject.SetActive(true);
+            GameOverPanel.SetActive(true);
         }
-        if (difficulty == 2)
+        else if (Player.position.z >= 3000)
         {
-            NPC2.gameObject.SetActive(true);
-            if (NPC2.position.z >= 857)
-            {
-                NPC2.gameObject.GetComponent<RunNariRun>().enabled = false;
-                RunBtnPanel.SetActive(false);
-                Lose_txt.gameObject.SetActive(true);
-                GameOverPanel.SetActive(true);
-            }
-            if (Player.position.z >= 857)
-            {
-                NPC2.gameObject.GetComponent<RunNariRun>().enabled = false;
-                RunBtnPanel.SetActive(false);
-                Win_txt.gameObject.SetActive(true);
-                GameOverPanel.SetActive(true);
-            }
+            nowNPC.gameObject.GetComponent<RunNPC>().enabled = false;
+            RunBtnPanel.SetActive(false);
+            Win_txt.gameObject.SetActive(true);
+            GameOverPanel.SetActive(true);
         }
-        if (difficulty == 3)
-        {
-            NPC3.gameObject.SetActive(true);
-            if (NPC3.position.z >= 857)
-            {
-                NPC3.gameObject.GetComponent<RunHimchanRun>().enabled = false;
-                RunBtnPanel.SetActive(false);
-                Lose_txt.gameObject.SetActive(true);
-                GameOverPanel.SetActive(true);
-            }
-            if (Player.position.z >= 857)
-            {
-                NPC3.gameObject.GetComponent<RunHimchanRun>().enabled = false;
-                RunBtnPanel.SetActive(false);
-                Win_txt.gameObject.SetActive(true);
-                GameOverPanel.SetActive(true);
-            }
-        }
+        NPCz = nowNPC.position.z;
+        Playerz = Player.position.z;
+
+        PMarker.localPosition = new Vector3(Playerz - 1600, 125, 0);
+        NMarker.localPosition = new Vector3(NPCz - 1600, 125, 0);
+
+        player.velocity = player.velocity / 1.0085f;
     }
 
     public void Reset()
@@ -94,27 +70,45 @@ public class RunGameManager : MonoBehaviour
         Vector3 startNpos = new Vector3(0.0f, 0.0f, 0.0f);
 
         Player.position = startPpos;
-        NPC1.position = startNpos;
-        NPC2.position = startNpos;
-        NPC3.position = startNpos;
+        NPC[0].position = startNpos;
+        NPC[1].position = startNpos;
+        NPC[2].position = startNpos;
 
         NMarker.localPosition = markerPos;
         PMarker.localPosition = markerPos;
 
-        NPC1.gameObject.SetActive(false);
-        NPC2.gameObject.SetActive(false);
-        NPC3.gameObject.SetActive(false);
+        NPC[0].gameObject.SetActive(false);
+        NPC[1].gameObject.SetActive(false);
+        NPC[2].gameObject.SetActive(false);
 
         difficulty = 0;
+        isPause = false;
 
         Win_txt.SetActive(false);
         Lose_txt.SetActive(false);
 
         GameOverPanel.SetActive(false);
         WelcomePanel.SetActive(true);
+        PausePanel.SetActive(false);
 
-        NPC1.gameObject.GetComponent<RunHamiRun>().enabled = true;
-        NPC2.gameObject.GetComponent<RunNariRun>().enabled = true;
-        NPC3.gameObject.GetComponent<RunHimchanRun>().enabled = true;
+        NPC[0].gameObject.GetComponent<RunNPC>().enabled = true;
+        NPC[1].gameObject.GetComponent<RunNPC>().enabled = true;
+        NPC[2].gameObject.GetComponent<RunNPC>().enabled = true;
+
+        player.velocity = new Vector3(0, 0, 0);
+    }
+
+    public void GamePause()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            PausePanel.SetActive(true);
+        }
+        else
+        {
+            PausePanel.SetActive(false);
+        }
+        
     }
 }
